148273dc0dc4622c01b1b2b51ce0fb6cc52fbed7
TeamDman <TeamDman9201@gmail.com>
2019-12-15 16:41:21 -0500

    Add weighted bag, add test for bag

---
+++ src/main/java/ca/teamdman/zensummoning/WeightedRandomBag.java
@@ -0,0 +1,43 @@
+package ca.teamdman.zensummoning;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.function.Predicate;
+
+public class WeightedRandomBag<T> {
+
+	private class Entry {
+		double accumulatedWeight;
+		T object;
+	}
+
+	private List<Entry> entries = new ArrayList<>();
+	private double accumulatedWeight;
+	private Random rand = new Random();
+
+	public void addEntry(T object, double weight) {
+		accumulatedWeight += weight;
+		Entry e = new Entry();
+		e.object = object;
+		e.accumulatedWeight = accumulatedWeight;
+		entries.add(e);
+	}
+
+	public T getRandom() {
+		return getRandom(null);
+	}
+
+	public T getRandom(Predicate<T> condition) {
+		double r = rand.nextDouble() * accumulatedWeight;
+
+		for (Entry entry: entries) {
+			if (condition == null || condition.test(entry.object)) {
+				if (entry.accumulatedWeight >= r) {
+					return entry.object;
+				}
+			}
+		}
+		return null; //should only happen when there are no entries
+	}
+}
